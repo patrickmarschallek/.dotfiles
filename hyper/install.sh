827976f9bca7bdb9169ff4340e2c97c7409376ab
Patrick Marschallek <patrick.marschallek@zalando.de>
2022-11-14 11:46:28 +0100

    initial commit

---
+++ hyper/install.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+installed_plugins=($(hyper ls))
+SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
+IFS=$'\n'      # Change IFS to newline char
+intsalled_plugins=($installed_plugins) # split the `names` string into an array by the same name
+IFS=$SAVEIFS   # Restore original IFS
+
+plugins=(hyper-font-ligatures hyper-one-dark hyperline)
+uninstall=(hyper-monokai-glow)
+
+rc_file=${HOME}/.npmrc
+rc_file_backup="${rc_file}.bck"
+
+# the custom npmrc file needs to be deactivated due to some errors 
+# in hyper for plugin installation via private repos
+[[ -f ${rc_file} ]] && mv $rc_file $rc_file_backup
+
+
+for plugin in "${plugins[@]}"
+do 
+  if [[ ${installed_plugins[*]} =~ ${plugin} ]]
+  then
+    echo "[Installed] '${plugin}'"
+  else
+    echo "[Installing] ${plugin}"
+    hyper install $plugin
+  fi
+
+done
+
+for plugin in "${uninstall[@]}"
+do 
+  if [[ ${installed_plugins[*]} =~ ${plugin} ]]
+  then
+    echo "[Uninstalling] '${plugin}'"
+    hyper uninstall $plugin
+  fi
+
+done
+
+[[ -f ${rc_file_backup} ]] && mv $rc_file_backup $rc_file
+
---
+++ macos/set-defaults.sh
@@ -0,0 +1,18 @@
+# Sets reasonable macOS defaults.
+#
+# Or, in other words, set shit how I like in macOS.
+#
+# The original idea (and a couple settings) were grabbed from:
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#
+# Run ./set-defaults.sh and you'll be good to go.
+
+# Always open everything in Finder's list view. This is important.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Set the Finder prefs for showing a few different volumes on the Desktop.
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Disable automatically rearrange Spaces based on recent use
+defaults write com.apple.dock mru-spaces -bool false
